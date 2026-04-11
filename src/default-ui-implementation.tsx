19e3702bf45d0185f59ffc5608ef22903f985411
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-04-12 02:27:12 +0600

    Working on Button Group

--- src/default-ui-implementation.tsx
+++ src/default-ui-implementation.tsx
@@ -2,12 +2,14 @@ import {UIActionSpec, UIAdapterData, UIComponentSpec} from "mfront-ui";
 import {defaultToastAction, DefaultToastBox} from "./default-ui/default-toast-box";
 import {DefaultButton} from "./default-ui/default-button";
 import {DefaultSeparator} from "./default-ui/default-separator";
+import {DefaultButtonGroup} from "./default-ui/default-button-group";
 
 
 export const DefaultUIComponent: UIComponentSpec = {
     ToastBox: DefaultToastBox,
     Button: DefaultButton,
-    Separator: DefaultSeparator
+    Separator: DefaultSeparator,
+    ButtonGroup: DefaultButtonGroup
 }
 
 export const DefaultUIAction: UIActionSpec = {
