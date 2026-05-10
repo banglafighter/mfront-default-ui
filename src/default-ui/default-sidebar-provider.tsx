4cb3bc4559162e8a0b60c336f66f40ded75d9437
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-11 02:01:02 +0600

    Working on UI Integration

--- src/default-ui/default-sidebar-provider.tsx
+++ src/default-ui/default-sidebar-provider.tsx
@@ -1,9 +1,90 @@
-import {WebSidebarProviderProps} from "mmcore-ui";
+import {WebSidebarContexProps, WebSidebarProviderProps} from "mmcore-ui";
 import {mergeWind} from "../common/tailwind-utils";
 
+import {
+    mmReactCreateContext,
+    MMReactCSSProperties,
+    mmReactUseCallback,
+    mmReactUseContext, mmReactUseEffect, mmReactUseMemo,
+    mmReactUseState
+} from "mmcore";
+import {MFrontException} from "mfront-core";
+import {useUIUtil} from "mfront-ui";
 
-export function DefaultSidebarProvider({className, ...props}: WebSidebarProviderProps) {
+const SidebarContext = mmReactCreateContext<WebSidebarContexProps | null>(null)
+
+export function useSidebarContext() {
+    const context = mmReactUseContext(SidebarContext)
+    if (!context) {
+        throw new MFrontException("useSidebar must be used within a SidebarProvider.")
+    }
+
+    return context
+}
+
+
+export function DefaultSidebarProvider(
+    {
+        className,
+        isOpen = true,
+        cookieName = "sidebar-state",
+        cookieMaxAge = 60 * 60 * 24 * 7,
+        sidebarWidth = "16rem",
+        sidebarWidthMobile = "18rem",
+        sidebarIconWidth = "3rem",
+        sidebarShortcutKey = "b",
+        children,
+        style,
+        ...props
+    }: WebSidebarProviderProps) {
+    const {isMobile} = useUIUtil()
+    const [mobileOpenState, setMobileOpenState] = mmReactUseState(false)
+
+    const setOtherOpenState = mmReactUseCallback((state: boolean) => {
+        // document.cookie = `${cookieName}=${openState}; path=/; max-age=${cookieMaxAge}`
+    }, [])
+
+    const toggleSidebar = mmReactUseCallback(() => {
+
+    }, [])
+
+    mmReactUseEffect(() => {
+        const handleKeyDown = (event: KeyboardEvent) => {
+            if (event.key === sidebarShortcutKey && (event.metaKey || event.ctrlKey)) {
+                event.preventDefault()
+                toggleSidebar()
+            }
+        }
+        window.addEventListener("keydown", handleKeyDown)
+        return () => window.removeEventListener("keydown", handleKeyDown)
+    }, [toggleSidebar])
+
+    const contextValue = mmReactUseMemo<WebSidebarContexProps>(() => (
+        {
+            isMobile,
+            toggleSidebar
+        }
+    ), [])
+
     return (
-        <></>
+        <SidebarContext.Provider value={contextValue}>
+            <div
+                data-tag={"sidebar-provider"}
+                style={
+                    {
+                        "--sidebar-width": sidebarWidth,
+                        "--sidebar-width-icon": sidebarIconWidth,
+                        ...style,
+                    } as MMReactCSSProperties
+                }
+                className={mergeWind(
+                    "group/sidebar-wrapper flex min-h-svh w-full has-data-[variant=inset]:bg-sidebar",
+                    className
+                )}
+                {...props}
+            >
+                {children}
+            </div>
+        </SidebarContext.Provider>
     )
 }
--- src/default-ui/default-sidebar.tsx
+++ src/default-ui/default-sidebar.tsx
@@ -1,9 +1,83 @@
 import {WebSidebarProps} from "mmcore-ui";
 import {mergeWind} from "../common/tailwind-utils";
+import {useSidebarContext} from "./default-sidebar-provider";
 
 
-export function DefaultSidebar({className, ...props}: WebSidebarProps) {
+export function DefaultSidebar({...props}: WebSidebarProps) {
     return (
-        <></>
+        <Sidebar {...props}>
+
+        </Sidebar>
     )
 }
+
+function Sidebar({className,  side = "left", variant = "sidebar", collapsible = "offcanvas", children, ...props}: WebSidebarProps) {
+    const {isMobile} = useSidebarContext()
+
+    if (collapsible === "none") {
+        return (
+            <div
+                data-tag="sidebar"
+                className={mergeWind(
+                    "flex h-full w-(--sidebar-width) flex-col bg-sidebar text-sidebar-foreground",
+                    className
+                )}
+                {...props}
+            >
+                {children}
+            </div>
+        )
+    }
+
+    if (isMobile) {
+        // load dialog box with left sheet
+    }
+
+    return (
+        <div
+            data-tag="sidebar"
+            className={mergeWind(
+                "group peer hidden text-sidebar-foreground md:block",
+                className
+            )}
+        >
+            <div
+                data-tag="sidebar-gap"
+                className={mergeWind(
+                    "relative w-(--sidebar-width) bg-transparent transition-[width] duration-200 ease-linear",
+                    "group-data-[collapsible=offcanvas]:w-0",
+                    "group-data-[side=right]:rotate-180",
+                    variant === "floating" || variant === "inset"
+                        ? "group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4)))]"
+                        : "group-data-[collapsible=icon]:w-(--sidebar-width-icon)"
+                )}
+            />
+            <div
+                data-tag="sidebar-container"
+                className={mergeWind(
+                    "fixed inset-y-0 z-10 hidden h-svh w-(--sidebar-width) transition-[left,right,width] duration-200 ease-linear md:flex",
+                    side === "left"
+                        ? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
+                        : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
+                    // Adjust the padding for floating and inset variants.
+                    variant === "floating" || variant === "inset"
+                        ? "p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4))+2px)]"
+                        : "group-data-[collapsible=icon]:w-(--sidebar-width-icon) group-data-[side=left]:border-r group-data-[side=right]:border-l",
+                    className
+                )}
+                {...props}
+            >
+                <div
+                    data-sidebar="sidebar"
+                    data-tag="sidebar-inner"
+                    className="flex h-full w-full flex-col bg-sidebar group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow-sm"
+                >
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
