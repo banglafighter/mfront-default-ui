a36adbca383fc5dab29158f0a4c5c4991e802b8c
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-11 16:05:21 +0600

    Working on Sidebar

--- src/default-ui/default-sidebar.tsx
+++ src/default-ui/default-sidebar.tsx
@@ -1,10 +1,10 @@
-import {WebSidebarProps} from "mmcore-ui";
-import {mergeWind} from "../common/tailwind-utils";
+import {SidebarMenuItemBaseProps, SidebarMenuItemProps, WebSidebarProps} from "mmcore-ui";
+import {makeClassVariance, mergeWind} from "../common/tailwind-utils";
 import {useSidebarContext} from "./default-sidebar-provider";
 import {mmReactUseCallback, UIComponentProps} from "mmcore";
 
 
-export function DefaultSidebar({header, headerAttrs, footer, footerAttrs, body, bodyAttrs, ...props}: WebSidebarProps) {
+export function DefaultSidebar({menu, header, headerAttrs, footer, footerAttrs, body, bodyAttrs, menuBefore, menuAfter, ...props}: WebSidebarProps) {
 
     const headerContent = mmReactUseCallback(() => {
         if (!header) {
@@ -36,6 +36,13 @@ export function DefaultSidebar({header, headerAttrs, footer, footerAttrs, body, 
                 </SidebarBodyBlock>
             )
         }
+        return (
+            <SidebarBodyBlock {...bodyAttrs}>
+                {menuBefore}
+                {getSidebarMenu(menu)}
+                {menuAfter}
+            </SidebarBodyBlock>
+        )
     }, [body])
 
     return (
@@ -46,6 +53,87 @@ export function DefaultSidebar({header, headerAttrs, footer, footerAttrs, body, 
         </SidebarBlock>
     )
 }
+
+export function getSidebarMenu(menu?: SidebarMenuItemProps[]) {
+    return (
+        <>
+            {menu?.map((item: SidebarMenuItemProps, index: number) => {
+                if (item.group) {
+                    return (
+                        <SidebarGroupBlock>
+                            <SidebarGroupLabelBlock>{item.menuContent}</SidebarGroupLabelBlock>
+                            {getMenuAndNestingMenu(item.group)}
+                        </SidebarGroupBlock>
+                    )
+                }
+                return getMenuAndNestingMenu([item])
+            })}
+        </>
+    )
+}
+
+function getMenuAndNestingMenu(nested?: SidebarMenuItemProps[], keyIndex?: number) {
+    const getSubMenu = (submenu?: SidebarMenuItemBaseProps[]) => {
+        if (!submenu) {
+            return ""
+        }
+        return (
+            <SidebarMenuSubBlock key={"menu-subitem-" + keyIndex}>
+                {submenu?.map((item: SidebarMenuItemBaseProps, index: number) => {
+                    return (
+                        <SidebarMenuSubItemBlock key={index} {...item.menuContentAttrs}>
+                            <SidebarMenuSubActionBlock size={item.size} isActive={false}>{item.menuContent}</SidebarMenuSubActionBlock>
+                        </SidebarMenuSubItemBlock>
+                    )
+                })}
+            </SidebarMenuSubBlock>
+        )
+    }
+    return (
+        <SidebarMenuBlock>
+            {nested?.map((item: SidebarMenuItemProps, index: number) => {
+                return (
+                    <SidebarMenuItemBlock key={index} {...item.menuContentAttrs}>
+                        <SidebarMenuItemActionBlock variant={item.variant} size={item.size} isActive={false} menuContent={item.menuContent}>
+                            {item.menuContent}
+                        </SidebarMenuItemActionBlock>
+                        {item.menuNext ? (
+                            <SidebarMenuNextBlock showOnHover={item.menuNextShowOnHover} {...item.menuNextAttrs}>
+                                {item.menuNext}
+                            </SidebarMenuNextBlock>
+                        ) : ""}
+                        {getSubMenu(item.nested)}
+                    </SidebarMenuItemBlock>
+                )
+            })}
+        </SidebarMenuBlock>
+    )
+}
+
+
+
+function SidebarMenuNextBlock({ className, showOnHover = false, ...props }: UIComponentProps<"div"> & { showOnHover?: boolean }) {
+  return (
+    <div
+      data-slot="sidebar-menu-action"
+      data-sidebar="menu-action"
+      className={mergeWind(
+        "absolute top-1.5 right-1 flex aspect-square w-5 items-center justify-center rounded-md p-0 text-sidebar-foreground ring-sidebar-ring outline-hidden transition-transform peer-hover/menu-button:text-sidebar-accent-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0",
+        // Increases the hit area of the button on mobile.
+        "after:absolute after:-inset-2 md:after:hidden",
+        "peer-data-[size=sm]/menu-button:top-1",
+        "peer-data-[size=default]/menu-button:top-1.5",
+        "peer-data-[size=lg]/menu-button:top-2.5",
+        "group-data-[collapsible=icon]:hidden",
+        showOnHover &&
+          "group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 peer-data-[active=true]/menu-button:text-sidebar-accent-foreground data-[state=open]:opacity-100 md:opacity-0",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
 
 function SidebarBodyBlock({ className, ...props }: UIComponentProps<"div">) {
   return (
@@ -70,6 +158,21 @@ function SidebarGroupBlock({ className, ...props }: UIComponentProps<"div">) {
       {...props}
     />
   )
+}
+
+function SidebarGroupLabelBlock({className, ...props}: UIComponentProps<"div">) {
+    return (
+        <div
+            data-tag="sidebar-group-label"
+            data-sidebar="group-label"
+            className={mergeWind(
+                "flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-medium text-sidebar-foreground/70 ring-sidebar-ring outline-hidden transition-[margin,opacity] duration-200 ease-linear focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0",
+                "group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0",
+                className
+            )}
+            {...props}
+        />
+    )
 }
 
 function SidebarMenuBlock({ className, ...props }: UIComponentProps<"ul">) {
@@ -211,6 +314,61 @@ function SidebarBlock({className,  side = "left", variant = "sidebar", collapsib
             </div>
         </div>
     )
+}
+
+const sidebarMenuItemActionVariants = makeClassVariance(
+    "peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm ring-sidebar-ring outline-hidden transition-[width,height,padding] group-has-data-[sidebar=menu-action]/menu-item:pr-8 group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2! hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0",
+    {
+        variants: {
+            variant: {
+                default: "hover:bg-sidebar-accent hover:text-sidebar-accent-foreground",
+                outline:
+                    "bg-background shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))]",
+            },
+            size: {
+                default: "h-8 text-sm",
+                small: "h-7 text-xs",
+                large: "h-12 text-sm group-data-[collapsible=icon]:p-0!",
+            },
+        },
+        defaultVariants: {
+            variant: "default",
+            size: "default",
+        },
+    }
+)
+
+function SidebarMenuItemActionBlock({ isActive = false, variant = "default", size = "default", className, ...props }: SidebarMenuItemBaseProps & UIComponentProps<"div"> & { isActive?: boolean}) {
+  return (
+    <div
+      data-tag="sidebar-menu-button"
+      data-sidebar="menu-button"
+      data-size={size}
+      data-active={isActive}
+      className={mergeWind(sidebarMenuItemActionVariants({ variant, size }), className)}
+      {...props}
+    />
+  )
+}
+
+function SidebarMenuSubActionBlock({ size = "default", isActive = false, className, ...props }: UIComponentProps<"div"> & { size?: "small" | "default" | "large", isActive?: boolean }) {
+  return (
+    <div
+      data-tag="sidebar-menu-sub-button"
+      data-sidebar="menu-sub-button"
+      data-size={size}
+      data-active={isActive}
+      className={mergeWind(
+        "flex h-7 min-w-0 -translate-x-px items-center gap-2 overflow-hidden rounded-md px-2 text-sidebar-foreground ring-sidebar-ring outline-hidden hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 [&>svg]:text-sidebar-accent-foreground",
+        "data-[active=true]:bg-sidebar-accent data-[active=true]:text-sidebar-accent-foreground",
+        size === "small" && "text-xs",
+        size === "default" && "text-sm",
+        "group-data-[collapsible=icon]:hidden",
+        className
+      )}
+      {...props}
+    />
+  )
 }
 
 
