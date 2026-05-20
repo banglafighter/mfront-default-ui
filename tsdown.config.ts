25c4e38c857f454eec2bd282887f9f19e3560376
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-05-20 12:26:39 +0600

    Working on Release

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -6,38 +6,38 @@ export default defineConfig({
     entry: ['src/index.ts'],
     outDir: 'dist',
 
-    deps: {
-        alwaysBundle: [
-            /^@radix-ui\//,
-
-            "clsx",
-            "tailwind-merge",
-            "class-variance-authority",
-
-            "date-fns",
-            "@date-fns/tz",
-
-            "react-day-picker",
-            "react-dropzone",
-            "react-toastify",
-
-            "attr-accept",
-            "file-selector",
-            "object-assign",
-            "prop-types",
-            "react-is",
-            "tw-animate-css",
-            "use-sync-external-store",
-            "reselect",
-            "aria-hidden",
-            "react-remove-scroll",
-            "tslib",
-            "use-sidecar",
-            "react-style-singleton",
-            "get-nonce",
-            "use-callback-ref",
-            "detect-node-es"
-        ]
-    }
+    // deps: {
+    //     alwaysBundle: [
+    //         /^@radix-ui\//,
+    //
+    //         "clsx",
+    //         "tailwind-merge",
+    //         "class-variance-authority",
+    //
+    //         "date-fns",
+    //         "@date-fns/tz",
+    //
+    //         "react-day-picker",
+    //         "react-dropzone",
+    //         "react-toastify",
+    //
+    //         "attr-accept",
+    //         "file-selector",
+    //         "object-assign",
+    //         "prop-types",
+    //         "react-is",
+    //         "tw-animate-css",
+    //         "use-sync-external-store",
+    //         "reselect",
+    //         "aria-hidden",
+    //         "react-remove-scroll",
+    //         "tslib",
+    //         "use-sidecar",
+    //         "react-style-singleton",
+    //         "get-nonce",
+    //         "use-callback-ref",
+    //         "detect-node-es"
+    //     ]
+    // }
 
 })
